cab34719d1d350a79171a6d81e975b792a5c81bd
Andres Cristancho <afcristancho5@misena.edu.co>
2021-10-19 23:32:35 +0200

    Correcion de status 200 a 201

--- src/main/java/com/reto4/reto4/web/ClientControlador.java
+++ src/main/java/com/reto4/reto4/web/ClientControlador.java
@@ -7,6 +7,7 @@ import com.reto4.reto4.modelo.Client;
 import com.reto4.reto4.servicio.ClientServicio;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 // import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 // import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,7 +33,7 @@ public class ClientControlador {
     }
 
     @PostMapping("/save")
-    // @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.CREATED)
     public Client guardar(@RequestBody Client datos){
         return clientServicio.guardar(datos);
     }
